c5df26e53305bdc7a8bb92461570841342966833
savent404 <savent_gate@outlook.com>
2024-01-02 02:21:44 +0000

    intergrate cmake template

---
+++ ecounter/ec.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ec_init(void);
+
+void ec_deinit(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ ecounter/src/ec_api.c
@@ -0,0 +1,11 @@
+#include "ecounter/ec.h"
+
+void ec_init(void)
+{
+
+}
+
+void ec_deinit(void)
+{
+
+}
